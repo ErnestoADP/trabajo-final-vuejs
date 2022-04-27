9cee93cd9cbddbafd68ef3279d76cd25da69b0b8
Ernesto Diaz <ernesto.adp1@gmail.com>
2022-04-27 18:25:08 -0400

    precio numero

--- src/views/Entrega.vue
+++ src/views/Entrega.vue
@@ -1,76 +1,76 @@
 <template>
-
-<div class="entrega">
-   <b-container>
-    <h1 class="mt-4 bv-example-row" style="text-align: left">
-      Información de entrega
-    </h1>
-    <b-row class="my-1">
-      <b-col col lg="4" md="12">
-        <b-input-group prepend="Nombre" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-        <b-input-group prepend="Region" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-        <b-input-group prepend="Nombre de calle" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-        <b-input-group prepend="Dpto/Block/Piso" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-      </b-col>
-      <b-col cols lg="8" md="12">
-        <b-input-group prepend="Apellido" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-        <b-input-group prepend="Comuna" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-        <b-input-group prepend="Número de calle" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-        <b-input-group prepend="Ciudad" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-      </b-col>
-    </b-row>
-    <h1 class="mt-4 bv-example-row" style="text-align: left">
-      Información de contacto
-    </h1>
-    <b-row class="my-1">
-      <b-col col lg="4" md="12">
-        <b-input-group prepend="Correo electrónico" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-      </b-col>
-      <b-col cols lg="8" md="12">
-        <b-input-group prepend="Teléfono" class="mt-3">
-          <b-form-input></b-form-input>
-        </b-input-group>
-      </b-col>
-    </b-row>
-    <h1 class="mt-4 bv-example-row" style="text-align: left">
-      Información de contacto
-    </h1>
-    <b-row class="my-1">
-      <b-col col lg="4" md="12" style="display: flex;">
-        <img
-          src="../assets/img/icon-adidas-redcompra.png"
-          width="200px"
-          alt="Kitten"
-        />
-      </b-col>
-      <b-col cols lg="8" md="12"> </b-col>
-    </b-row>
-    <div style="float: left">
-      <b-button to="/Pago_exitoso" variant="outline-success" class="mt-4">Hacer pedido</b-button>
-    </div>
-  </b-container>
-
-</div>
- 
+  <div class="entrega">
+    <b-container>
+      <h1 class="mt-4 bv-example-row" style="text-align: left">
+        Información de entrega
+      </h1>
+      <b-row class="my-1">
+        <b-col col lg="4" md="12">
+          <b-input-group prepend="Nombre" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+          <b-input-group prepend="Region" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+          <b-input-group prepend="Nombre de calle" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+          <b-input-group prepend="Dpto/Block/Piso" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols lg="8" md="12">
+          <b-input-group prepend="Apellido" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+          <b-input-group prepend="Comuna" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+          <b-input-group prepend="Número de calle" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+          <b-input-group prepend="Ciudad" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+      </b-row>
+      <h1 class="mt-4 bv-example-row" style="text-align: left">
+        Información de contacto
+      </h1>
+      <b-row class="my-1">
+        <b-col col lg="4" md="12">
+          <b-input-group prepend="Correo electrónico" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+        <b-col cols lg="8" md="12">
+          <b-input-group prepend="Teléfono" class="mt-3">
+            <b-form-input></b-form-input>
+          </b-input-group>
+        </b-col>
+      </b-row>
+      <h1 class="mt-4 bv-example-row" style="text-align: left">
+        Información de contacto
+      </h1>
+      <b-row class="my-1">
+        <b-col col lg="4" md="12" style="display: flex">
+          <img
+            src="../assets/img/icon-adidas-redcompra.png"
+            width="200px"
+            alt="Kitten"
+          />
+        </b-col>
+        <b-col cols lg="8" md="12"> </b-col>
+      </b-row>
+      <div style="float: left">
+        <b-button to="/Pago_exitoso" variant="outline-success" class="mt-4"
+          >Hacer pedido</b-button
+        >
+      </div>
+    </b-container>
+  </div>
 </template>
+
 
 <style>
 .cuadro1 {
--- src/views/Pago_exitoso.vue
+++ src/views/Pago_exitoso.vue
@@ -1,44 +1,59 @@
 <template>
-<div class="pagos">
-   <b-container>
-    <b-row class="my-1 mt-4" style="background: white; border-radius: 10px">
-      <b-col col lg="4" md="12" class="cuadro1">
-        <h2>Lo que compraste</h2>
-        <hr />
-        <p>Libro1</p>
-        <p>Libro2</p>
-        <p>Libro3</p>
-        <hr />
-        <div style="display: flex">
-          <b-icon style="margin-right: 10px" icon="cash"></b-icon>
-          <b>$50.000</b>
-        </div>
-        <hr />
-        <div style="display: flex">
-          <b-icon style="margin-right: 10px" icon="calendar-check"></b-icon>
-          <p>Fecha de entrega:</p>
-          <b> 10 de Mayo 2022</b>
-        </div>
-      </b-col>
-      <b-col
-        cols lg="8" md="12"
-        class="cuadro2"
-      >
-        <h1 style="color: #d3362f; margin-bottom: 30px; font-weight: bold;">
-          PAGO REALIZADO CON EXITO
-        </h1>
-        <img src="../assets/img/11_Success-1.png" width="70%" alt="Kitten" />
-      </b-col>
-    </b-row>
-  </b-container>
-</div>
- 
+  <div class="pagos">
+    <b-container>
+      <b-row class="my-1 mt-4" style="background: white; border-radius: 10px">
+        <b-col col lg="4" md="12" class="cuadro1">
+          <h2>Lo que compraste</h2>
+          <hr />
+          <div v-for="item in productosComprados" :key="item.id">
+            <p>Numero: {{ item.numero }}</p>
+          </div>
+
+          <hr />
+          <div style="display: flex">
+            <b-icon style="margin-right: 10px" icon="cash"></b-icon>
+            <b>${{ precioFinal }}</b>
+          </div>
+          <hr />
+          <div style="display: flex">
+            <b-icon style="margin-right: 10px" icon="calendar-check"></b-icon>
+            <p>Fecha de entrega:</p>
+            <b> 10 de Mayo 2022</b>
+          </div>
+        </b-col>
+        <b-col cols lg="8" md="12" class="cuadro2">
+          <h1 style="color: #d3362f; margin-bottom: 30px; font-weight: bold">
+            PAGO REALIZADO CON EXITO
+          </h1>
+          <img src="../assets/img/11_Success-1.png" width="70%" alt="Kitten" />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      precioFinal: 0,
+      productosComprados: [],
+    };
+  },
+  mounted() {
+    this.productosComprados = JSON.parse(localStorage.getItem("carrito"));
+
+    this.productosComprados.forEach((element) => {
+      this.precioFinal += element.precio;
+    });
+  },
+};
+</script>
 
 <style>
 .cuadro1 {
   background: #d3362f;
-  padding: 20px 40px!important;
+  padding: 20px 40px !important;
   border-radius: 10px 0 0 10px;
   text-align: left;
   color: white;
@@ -47,10 +62,10 @@
 .cuadro2 {
   padding: 50px;
   border-radius: 10px 0 0 10px;
-  background-color:rgba(0, 0, 0, 0.439);
+  background-color: rgba(0, 0, 0, 0.439);
 }
 
 .pagos {
-    margin-bottom: 280px;
+  margin-bottom: 280px;
 }
 </style>
